7af6c0e9cea78de3cd60536eacef8b648d228ebf
Parth Gupta <parthgupta3003@gmail.com>
2023-04-10 21:53:20 +0530

    added audio capturer instead of video hax

--- AudioCapture.py
+++ AudioCapture.py
@@ -16,8 +16,6 @@ stream = audio.open(format=pyaudio.paInt16, channels=1, rate=RATE, input=True, f
 
 # Initialize video recording
 cap = cv2.VideoCapture(0)
-fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-out = cv2.VideoWriter(VIDEO_OUTPUT_FILENAME, fourcc, 20.0, (640,480))
 
 # Record audio and video until spacebar is pressed
 frames = []
@@ -33,7 +31,6 @@ while True:
     # Record video
     ret, frame = cap.read()
     if ret:
-        out.write(frame)
         cv2.imshow('Press Spacebar to Stop Recording', frame)
 
 
@@ -42,7 +39,6 @@ stream.stop_stream()
 stream.close()
 audio.terminate()
 cap.release()
-out.release()
 
 # Save audio as WAV file
 wavfile = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
@@ -51,9 +47,3 @@ wavfile.setsampwidth(audio.get_sample_size(pyaudio.paInt16))
 wavfile.setframerate(RATE)
 wavfile.writeframes(b''.join(frames))
 wavfile.close()
-
-# Combine audio and video into a single MP4 file
-audio_clip = mp.AudioFileClip(WAVE_OUTPUT_FILENAME)
-video_clip = mp.VideoFileClip(VIDEO_OUTPUT_FILENAME)
-final_clip = video_clip.set_audio(audio_clip)
-final_clip.write_videofile("final_output.mp4")
