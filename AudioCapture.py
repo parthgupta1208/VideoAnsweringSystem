56b6c09c61914c0e78e5dbf144adb282d13937fb
Parth Gupta <parthgupta3003@gmail.com>
2023-04-11 10:13:05 +0530

    improved audio quality (now stt working)

--- AudioCapture.py
+++ AudioCapture.py
@@ -3,6 +3,7 @@ import pyaudio
 import wave
 import numpy as np
 import moviepy.editor as mp
+import threading
 
 # Define constants for recording settings
 RATE = 44100
@@ -11,39 +12,54 @@ WAVE_OUTPUT_FILENAME = "audio.wav"
 VIDEO_OUTPUT_FILENAME = "output.mp4"
 
 # Initialize audio recording
-audio = pyaudio.PyAudio()
-stream = audio.open(format=pyaudio.paInt16, channels=1, rate=RATE, input=True, frames_per_buffer=CHUNK)
+audio_frames = []
+audio_event = threading.Event()
+
+def record_audio():
+    audio = pyaudio.PyAudio()
+    stream = audio.open(format=pyaudio.paInt16, channels=1, rate=RATE, input=True, frames_per_buffer=CHUNK)
+
+    while not audio_event.is_set():
+        data = stream.read(CHUNK)
+        audio_frames.append(np.frombuffer(data, dtype=np.int16))
+
+    stream.stop_stream()
+    stream.close()
+    audio.terminate()
 
 # Initialize video recording
-cap = cv2.VideoCapture(0)
+video_frames = []
+video_event = threading.Event()
 
-# Record audio and video until spacebar is pressed
-frames = []
-while True:
-    # Check if spacebar has been pressed
-    if cv2.waitKey(1) & 0xFF == ord(' '):
-        break
+def show_video():
+    cap = cv2.VideoCapture(0)
 
-    # Record audio
-    data = stream.read(CHUNK)
-    frames.append(np.frombuffer(data, dtype=np.int16))
+    while not video_event.is_set():
+        ret, frame = cap.read()
+        if ret:
+            video_frames.append(frame)
+            cv2.imshow('Press Spacebar to Stop Recording', frame)
+            if cv2.waitKey(1) & 0xFF == ord(' '):
+                break
 
-    # Record video
-    ret, frame = cap.read()
-    if ret:
-        cv2.imshow('Press Spacebar to Stop Recording', frame)
+    cap.release()
+    cv2.destroyAllWindows()
 
+    # Stop recording and save audio and video files
+    video_event.set()
+    audio_event.set()
+    audio_thread.join()
 
-# Stop recording
-stream.stop_stream()
-stream.close()
-audio.terminate()
-cap.release()
+    # Save the wav file 
+    wavfile = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+    wavfile.setnchannels(1)
+    wavfile.setsampwidth(2)
+    wavfile.setframerate(RATE)
+    wavfile.writeframes(b''.join(audio_frames))
+    wavfile.close()
 
-# Save audio as WAV file
-wavfile = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
-wavfile.setnchannels(1)
-wavfile.setsampwidth(audio.get_sample_size(pyaudio.paInt16))
-wavfile.setframerate(RATE)
-wavfile.writeframes(b''.join(frames))
-wavfile.close()
+# Start audio recording and video showing threads
+audio_thread = threading.Thread(target=record_audio)
+video_thread = threading.Thread(target=show_video)
+audio_thread.start()
+video_thread.start()
--- VideoCapture.py
+++ VideoCapture.py
@@ -3,6 +3,7 @@ import pyaudio
 import wave
 import numpy as np
 import moviepy.editor as mp
+import threading
 
 # Define constants for recording settings
 RATE = 44100
@@ -11,49 +12,62 @@ WAVE_OUTPUT_FILENAME = "audio.wav"
 VIDEO_OUTPUT_FILENAME = "output.mp4"
 
 # Initialize audio recording
-audio = pyaudio.PyAudio()
-stream = audio.open(format=pyaudio.paInt16, channels=1, rate=RATE, input=True, frames_per_buffer=CHUNK)
+audio_frames = []
+audio_event = threading.Event()
+
+def record_audio():
+    audio = pyaudio.PyAudio()
+    stream = audio.open(format=pyaudio.paInt16, channels=1, rate=RATE, input=True, frames_per_buffer=CHUNK)
+
+    while not audio_event.is_set():
+        data = stream.read(CHUNK)
+        audio_frames.append(np.frombuffer(data, dtype=np.int16))
+
+    stream.stop_stream()
+    stream.close()
+    audio.terminate()
 
 # Initialize video recording
-cap = cv2.VideoCapture(0)
-fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-out = cv2.VideoWriter(VIDEO_OUTPUT_FILENAME, fourcc, 20.0, (640,480))
+video_frames = []
+video_event = threading.Event()
 
-# Record audio and video until spacebar is pressed
-frames = []
-while True:
-    # Check if spacebar has been pressed
-    if cv2.waitKey(1) & 0xFF == ord(' '):
-        break
+def record_video():
+    cap = cv2.VideoCapture(0)
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    out = cv2.VideoWriter(VIDEO_OUTPUT_FILENAME, fourcc, 20.0, (640,480))
 
-    # Record audio
-    data = stream.read(CHUNK)
-    frames.append(np.frombuffer(data, dtype=np.int16))
+    while not video_event.is_set():
+        ret, frame = cap.read()
+        if ret:
+            video_frames.append(frame)
+            out.write(frame)
+            cv2.imshow('Press Spacebar to Stop Recording', frame)
+            if cv2.waitKey(1) & 0xFF == ord(' '):
+                break
 
-    # Record video
-    ret, frame = cap.read()
-    if ret:
-        out.write(frame)
-        cv2.imshow('Press Spacebar to Stop Recording', frame)
+    cap.release()
+    out.release()
+    cv2.destroyAllWindows()
 
+    # Stop recording and save audio and video files
+    video_event.set()
+    audio_event.set()
+    audio_thread.join()
 
-# Stop recording
-stream.stop_stream()
-stream.close()
-audio.terminate()
-cap.release()
-out.release()
+    wavfile = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+    wavfile.setnchannels(1)
+    wavfile.setsampwidth(2)
+    wavfile.setframerate(RATE)
+    wavfile.writeframes(b''.join(audio_frames))
+    wavfile.close()
 
-# Save audio as WAV file
-wavfile = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
-wavfile.setnchannels(1)
-wavfile.setsampwidth(audio.get_sample_size(pyaudio.paInt16))
-wavfile.setframerate(RATE)
-wavfile.writeframes(b''.join(frames))
-wavfile.close()
+    audio_clip = mp.AudioFileClip(WAVE_OUTPUT_FILENAME)
+    video_clip = mp.concatenate_videoclips([mp.VideoFileClip(VIDEO_OUTPUT_FILENAME)])
+    final_clip = video_clip.set_audio(audio_clip)
+    final_clip.write_videofile("final_output.mp4")
 
-# Combine audio and video into a single MP4 file
-audio_clip = mp.AudioFileClip(WAVE_OUTPUT_FILENAME)
-video_clip = mp.VideoFileClip(VIDEO_OUTPUT_FILENAME)
-final_clip = video_clip.set_audio(audio_clip)
-final_clip.write_videofile("final_output.mp4")
+# Start audio and video recording threads
+audio_thread = threading.Thread(target=record_audio)
+video_thread = threading.Thread(target=record_video)
+audio_thread.start()
+video_thread.start()
