e8dd0ded20e7b3c1c8029ce96a8f1961da7d7c02
Parth Gupta <parthgupta3003@gmail.com>
2023-04-13 02:05:37 +0530

    almost final

--- FlaskApp2.py
+++ FlaskApp2.py
@@ -21,7 +21,18 @@ def hello():
 
 @app.route('/Text', methods=['POST'])
 def processtext():
-    pass
+    text = request.form['textboxinputdata']
+    openai.api_key = os.getenv("OPENAI_KEY")
+    completion = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo", 
+    messages = [{"role": "system", "content" : "You are FridayAI, a large language model trained by Parth Gupta. Answer as concisely as possible.\nKnowledge cutoff: 2021-09-01\nCurrent date: 2023-04-10"},
+    {"role": "user", "content" : "How are you?"},
+    {"role": "assistant", "content" : "I am doing well"},
+    {"role": "user", "content" : text}]
+    )
+    print(completion['choices'][0]['message']['content'])
+    html = markdown2.markdown(completion['choices'][0]['message']['content'])
+    return render_template("result.html", textboxdata=html)
 
 @app.route('/Select', methods=['POST'])
 def upload():
